4cc93ca6a3135ad526108c7a0106f7d157a2b0c8
XeNoXTS <palmyunvisate2003@gmail.com>
2023-03-10 15:37:02 +0700

    lab25_3 Completed

--- lab25_3.cpp
+++ lab25_3.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
-//Write function count() here.
+int count(int data[],int N){
+	set<int> NumSet;
+	for(int i = 0;i < N;i++){
+		NumSet.insert(data[i]);
+	}
+	return NumSet.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
